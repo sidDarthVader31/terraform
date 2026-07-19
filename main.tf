4e524defe3b63e17bf3d648756726d96cc8505b5
Siddharth Bisht <siddharthbisht31@gmail.com>
2026-07-19 23:23:36 +0530

    add auto scaling

--- main.tf
+++ main.tf
@@ -13,25 +13,25 @@ data "aws_ami" "app_ami" {
   }
 }
 
-
-resource "aws_instance" "blog" {
-  ami           = data.aws_ami.app_ami.id
-  instance_type = var.instance_type
-
-  # Bootstrap script to install Tomcat automatically
-  user_data = <<-EOF
-              #!/bin/bash
-              sudo apt-get update -y
-              sudo apt-get install tomcat9 tomcat9-admin -y
-              EOF
-
-  vpc_security_group_ids = [module.blog_sg.id]
-  subnet_id = module.blog_vpc.public_subnets[0]
-  tags = {
-    Name = "learning terraform"
-  }
-}
-
+#
+# resource "aws_instance" "blog" {
+#   # ami           = data.aws_ami.app_ami.id
+#   instance_type = var.instance_type
+#
+#   # Bootstrap script to install Tomcat automatically
+#   user_data = <<-EOF
+#               #!/bin/bash
+#               sudo apt-get update -y
+#               sudo apt-get install tomcat9 tomcat9-admin -y
+#               EOF
+#
+#   # vpc_security_group_ids = [module.blog_sg.id]
+#   subnet_id = module.blog_vpc.public_subnets[0]
+#   tags = {
+#     Name = "learning terraform"
+#   }
+# }
+#
 
 module "blog_vpc" {
   source = "terraform-aws-modules/vpc/aws"
@@ -147,10 +147,36 @@ resource "aws_lb_target_group" "blog" {
   protocol = "HTTP"
   vpc_id   = module.blog_vpc.vpc_id
 }
+#
+#
+# resource "aws_lb_target_group_attachment" "blog" {
+#   target_group_arn = aws_lb_target_group.blog.arn
+#   target_id        = aws_instance.blog.id
+#   port             = 80
+# }
 
 
-resource "aws_lb_target_group_attachment" "blog" {
-  target_group_arn = aws_lb_target_group.blog.arn
-  target_id        = aws_instance.blog.id
-  port             = 80
+module "autoscaling" {
+  source  = "terraform-aws-modules/autoscaling/aws"
+  version = "9.2.1"
+  
+  name = "blog"
+  min_size = 1
+  max_size = 2
+
+  vpc_zone_identifier = module.blog_vpc.public_subnets
+  
+  launch_template_name = "blog"
+
+  security_groups = [module.blog_sg.id]
+
+  instance_type = var.instance_type
+
+  image_id = data.aws_ami.app_ami.id
+
+  traffic_source_attachments = {
+    blog-alb = {
+      traffic_source_identifier = aws_lb_target_group.blog.arn
+    }
+  }
 }
