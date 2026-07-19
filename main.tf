b3ea1b7b45002f3f1d7ffd37dc20856dc638e042
Siddharth Bisht <siddharthbisht31@gmail.com>
2026-07-19 21:44:06 +0530

    add security group

--- main.tf
+++ main.tf
@@ -12,8 +12,11 @@ data "aws_ami" "app_ami" {
     values = ["hvm"]
   }
 }
+data "aws_vpc" default {
+  default = true
+}
 
-resource "aws_instance" "web" {
+resource "aws_instance" "blog" {
   ami           = data.aws_ami.app_ami.id
   instance_type = var.instance_type
 
@@ -24,8 +27,48 @@ resource "aws_instance" "web" {
               sudo apt-get install tomcat9 tomcat9-admin -y
               EOF
 
+  vpc_security_group_ids = [aws_security_group.blog.id]
   tags = {
-    Name = "HelloWorld"
+    Name = "learning terraform"
   }
 }
 
+
+resource "aws_security_group" "blog" {
+  name          = "blog"
+  description   = "allow http and https in. Allow everything out"
+  vpc_id = data.aws_vpc.default.id
+}
+
+
+
+resource "aws_security_group_rule" "blog_http_in" {
+  type = "ingress"
+  from_port = 80
+  to_port = 80
+  protocol = "tcp"
+  cidr_blocks = ["0.0.0.0/0"]
+
+  security_group_id = aws_security_group.blog.id
+}
+
+
+resource "aws_security_group_rule" "blog_https_in" {
+  type = "ingress"
+  from_port = 80
+  to_port = 80
+  protocol = "tcp"
+  cidr_blocks = ["0.0.0.0/0"]
+
+  security_group_id = aws_security_group.blog.id
+}
+
+resource "aws_security_group_rule" "blog_everything_out" {
+  type = "egress"
+  from_port = 0
+  to_port = 0
+  protocol = -1 
+  cidr_blocks = ["0.0.0.0/0"]
+
+  security_group_id = aws_security_group.blog.id
+}
