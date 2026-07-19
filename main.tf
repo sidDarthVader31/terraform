11e043a08095dfd99192d64ca64dcb6da3a14274
Siddharth Bisht <siddharthbisht31@gmail.com>
2026-07-19 11:48:22 +0530

    Modify AMI filter and add Tomcat installation script
    Updated the AMI filter to use Ubuntu Jammy and added a bootstrap script for Tomcat installation.

--- main.tf
+++ main.tf
@@ -1,24 +1,31 @@
 data "aws_ami" "app_ami" {
   most_recent = true
+  owners      = ["099720109477"] # Official Canonical Ubuntu Owner ID
 
   filter {
     name   = "name"
-    values = ["bitnami-tomcat-*-x86_64-hvm-ebs-nami"]
+    values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*"]
   }
 
   filter {
     name   = "virtualization-type"
     values = ["hvm"]
   }
-
-  owners = ["979382823631"] # Bitnami
 }
 
 resource "aws_instance" "web" {
   ami           = data.aws_ami.app_ami.id
   instance_type = "t3.nano"
 
+  # Bootstrap script to install Tomcat automatically
+  user_data = <<-EOF
+              #!/bin/bash
+              sudo apt-get update -y
+              sudo apt-get install tomcat9 tomcat9-admin -y
+              EOF
+
   tags = {
     Name = "HelloWorld"
   }
 }
+
