fc5a57c8f4541d895dff5af59455d6815aebd9e7
Siddharth Bisht <siddharthbisht31@gmail.com>
2026-07-19 23:42:08 +0530

    refractor code to use variables

--- main.tf
+++ main.tf
@@ -1,10 +1,10 @@
 data "aws_ami" "app_ami" {
   most_recent = true
-  owners      = ["099720109477"] # Official Canonical Ubuntu Owner ID
+  owners      = [var.ami_filter.owner] # Official Canonical Ubuntu Owner ID
 
   filter {
     name   = "name"
-    values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*"]
+    values = [var.ami_filter.name]
   }
 
   filter {
@@ -35,18 +35,18 @@ data "aws_ami" "app_ami" {
 
 module "blog_vpc" {
   source = "terraform-aws-modules/vpc/aws"
-  name = "dev"
-  cidr = "10.0.0.0/16"
+  name = var.environment.name 
+  cidr = "${var.environment.network_prefix}.0.0/16"
 
   azs             = ["ap-south-1a", "ap-south-1b", "ap-south-1c"]
-  private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
-  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
+  private_subnets = ["${var.environment.network_prefix}.1.0/24", "${var.environment.network_prefix}.2.0/24", "${var.environment.network_prefix}.3.0/24"]
+  public_subnets  = ["${var.environment.network_prefix}.101.0/24", "${var.environment.network_prefix}.102.0/24", "${var.environment.network_prefix}.103.0/24"]
 
   enable_nat_gateway = true
 
   tags = {
     Terraform = "true"
-    Environment = "dev"
+    Environment = var.environment.name 
   }
 }
 #
@@ -110,14 +110,13 @@ module "blog_sg" {
   }
 
   tags = {
-    Environment = "dev"
+    Environment = var.environment.name
   }
 }
 
 
 module "blog_alb" {
   source = "terraform-aws-modules/alb/aws"
-
   name    = "blog-alb"
   vpc_id  = module.blog_vpc.vpc_id
   subnets = module.blog_vpc.public_subnets
@@ -161,8 +160,8 @@ module "autoscaling" {
   version = "9.2.1"
   
   name = "blog"
-  min_size = 1
-  max_size = 2
+  min_size = var.min_size
+  max_size = var.max_size
 
   vpc_zone_identifier = module.blog_vpc.public_subnets
   
--- variables.tf
+++ variables.tf
@@ -2,3 +2,42 @@ variable "instance_type" {
  description = "Type of EC2 instance to provision"
  default     = "t3.nano"
 }
+
+
+variable "ami_filter"  {
+  description = "name filter and owner for ami"
+  type = object({
+    name = string
+    owner = string
+  })
+
+  default = {
+    name = "ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*" 
+    owner      = "099720109477" # Official Canonical Ubuntu Owner ID
+  }
+}
+
+variable "environment" {
+  type = object({
+    name = string
+    network_prefix = string
+  })
+  default = {
+    name = "dev"
+    network_prefix = "10.0"
+  }
+  description = "deployment environment"
+}
+
+
+variable "min_size" {
+  default = 1
+  description = "minimum no of instances in asg"
+}
+
+variable "max_size" {
+  default = 2
+  description = "maxmimum no of instances in asg"
+  
+}
+
